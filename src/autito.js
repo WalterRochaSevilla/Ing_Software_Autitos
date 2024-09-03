8b346838b43df61bc4dc07dd5a3a947dc58b584e
FernandoNB06 <fernando.nogales@ucb.edu.bo>
2024-09-03 09:32:43 -0400

    posicion inicial autito

---
+++ src/autito.js
@@ -0,0 +1,4 @@
+function autito(x,y,O){
+    return {X: x, Y: y, O: O};
+}
+export default autito;
--- src/autitos.test.js
+++ src/autitos.test.js
@@ -1,9 +1,16 @@
 import Grid from "./grid";
+import autito from "./autito";
 describe("Posición", () => {
     it("deberia Devolver la Posicion", () => {
       const grid = new Grid();
       grid.setAtr(3,3);
       expect(grid.getAtr()).toEqual({ x: 3, y: 3 });
+    });
+  });
+
+  describe("Autito", () => {
+    it("deberia Devolver la Posicion Inicial del autito", () => {
+      expect(autito(0,3,"N")).toEqual( {X :0 , Y: 3 , O: "N" } );
     });
   });
   
